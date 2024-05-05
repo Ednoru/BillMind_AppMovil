194c1d5d8e99891b4fa3937727d150c22ebf987c
Eduardo Chero <eduardochero08@gmail.com>
2024-05-05 16:26:28 -0500

    commit

--- app/src/main/java/pe/edu/upc/billmind/ui/screens/home/AlertsScreen.kt
+++ app/src/main/java/pe/edu/upc/billmind/ui/screens/home/AlertsScreen.kt
@@ -1,7 +1,6 @@
 package pe.edu.upc.billmind.ui.screens.home
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import pe.edu.upc.billmind.ui.shared.BackgroundImg
--- app/src/main/java/pe/edu/upc/billmind/ui/screens/home/DebtsScreen.kt
+++ app/src/main/java/pe/edu/upc/billmind/ui/screens/home/DebtsScreen.kt
@@ -1,8 +1,6 @@
 package pe.edu.upc.billmind.ui.screens.home
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
